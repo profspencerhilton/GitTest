717f356d3bf75e611e1faaa7cd4f5f1fb184486b
profspencerhilton <spencer.hilton@byu.edu>
2025-01-21 10:04:35 -0700

    Hey, I want to add some code too.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Here is a second line of code");
+Console.WriteLine("Here is a third line of code");
