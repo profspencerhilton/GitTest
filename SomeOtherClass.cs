d7036267f3be5dab8f6d008518ac3d2c753a28cc
Record <Record@SPENCER-XPS8930>
2025-01-21 10:41:45 -0700

    Trying to do things the right way

--- SomeOtherClass.cs
+++ SomeOtherClass.cs
@@ -7,5 +7,7 @@ public class SomeOtherClass
         Console.WriteLine("At the same time Evil Hilton is making a change");
         Console.WriteLine("Prof. Hilton is making a change to the code");
 
+        Console.WriteLine("Let's do things the right way");
+
     }
 }
